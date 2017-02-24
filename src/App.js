712011b9206dc1144ff1a874d9082890b70b0fd4
fresh5447 <doug@kosmojo.com>
2017-02-24 10:49:30 -0700

    revert code back to starting point

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import DATA from './data/cars';
-import CarsList from './CarsList';
 
 var App = React.createClass({
-  getInitialState: function(){
-    return {
-      cars: DATA.cars,
-      categories: DATA.categories
-    }
-  },
   render: function(){
-    var filteredCars = this.state.cars.filter(function(c){
-      return c.brand === "porsche";
-    });
     return(
       <div>
         <div className="jumbotron">
@@ -20,11 +9,9 @@ var App = React.createClass({
         </div>
         <div className="jumbotron">
           <h3>All My Cars</h3>
-          <CarsList cars={this.state.cars} />
         </div>
         <div className="jumbotron">
           <h3>Cars Filtered</h3>
-          <CarsList cars={filteredCars} />
         </div>
       </div>
     )
