e9e75604fe5e1caa11ac8ebddcddf0594320518e
fresh5447 <doug@kosmojo.com>
2017-02-24 09:52:16 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var App = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h3>Hello From App!</h3>
+      </div>
+    )
+  }
+});
+
+export default App;
